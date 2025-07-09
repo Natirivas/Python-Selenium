0f43d9f29f3356fbe07bba3012fd07b279709f2a
Natalia Rivas <161992123+Natirivas@users.noreply.github.com>
2025-07-09 03:28:04 -0300

    main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
 
-#https://googlechromelabs.github.io/chrome-for-testing/
-
 service = Service(executable_path="chromedriver.exe" )
 driver = webdriver.Chrome(service=service)
 
